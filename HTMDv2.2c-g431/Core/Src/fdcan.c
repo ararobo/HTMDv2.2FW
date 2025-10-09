7790a60b0ab678ee2250bbc7f769d4583672c544
aiba-gento <8gn24gn25@gmail.com>
2025-10-09 09:43:57 +0900

    update:can segment

--- HTMDv2.2c-g431/Core/Src/fdcan.c
+++ HTMDv2.2c-g431/Core/Src/fdcan.c
@@ -46,8 +46,8 @@ void MX_FDCAN1_Init(void)
   hfdcan1.Init.ProtocolException = DISABLE;
   hfdcan1.Init.NominalPrescaler = 4;
   hfdcan1.Init.NominalSyncJumpWidth = 1;
-  hfdcan1.Init.NominalTimeSeg1 = 13;
-  hfdcan1.Init.NominalTimeSeg2 = 2;
+  hfdcan1.Init.NominalTimeSeg1 = 11;
+  hfdcan1.Init.NominalTimeSeg2 = 4;
   hfdcan1.Init.DataPrescaler = 1;
   hfdcan1.Init.DataSyncJumpWidth = 1;
   hfdcan1.Init.DataTimeSeg1 = 1;
