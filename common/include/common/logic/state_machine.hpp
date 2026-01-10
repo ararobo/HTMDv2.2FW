3cdff77217dae4b4d758a12f5d080f3680a61b44
aiba-gento <8gn24gn25@gmail.com>
2026-01-10 12:00:58 +0900

    Add StateMachine class and SystemState enum for state management

---
+++ common/include/common/logic/state_machine.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <cstdint>
+
+namespace common::logic {
+
+enum class SystemState : uint8_t {
+    INITIALIZATION,
+    IDLE,
+    PROCESSING,
+    ERROR,
+};
+
+class StateMachine {
+  public:
+    StateMachine() : current_state_(SystemState::INITIALIZATION) {}
+
+    SystemState get_state() const { return current_state_; }
+
+    void set_state(SystemState new_state) { current_state_ = new_state; }
+
+  private:
+    SystemState current_state_;
+};
+}  // namespace common::logic
