50e370a0ab3a7429d32b3b5c308feb89a95b2934
aiba-gento <8gn24gn25@gmail.com>
2026-01-14 18:14:36 +0900

    Add EncoderInterface and IndicatorInterface with necessary methods and enums

---
+++ common/include/common/interfaces/encoder_interface.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cfloat>
+#include <cstdint>
+
+namespace common::interfaces {
+class EncoderInterface {
+  public:
+    virtual bool init() = 0;
+
+    virtual int32_t get_counts() = 0;
+
+    virtual float get_velocity() = 0;
+
+    virtual void reset_counts() = 0;
+};
+
+}  // namespace common::interfaces
