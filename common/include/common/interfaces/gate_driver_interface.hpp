793c6aedd967220313b5b71394b1ccb8ce8f7207
gn10g <8gn24gn25@gmail.com>
2026-01-14 22:42:20 +0900

    Refactor EncoderInterface and GateDriverInterface to improve code readability; add missing documentation comments Update gn10_can submodule to latest commit

--- common/include/common/interfaces/gate_driver_interface.hpp
+++ common/include/common/interfaces/gate_driver_interface.hpp
@@ -8,9 +8,16 @@ namespace common::interfaces {
 class GateDriverInterface {
   public:
     virtual ~GateDriverInterface() = default;
-    
+
     virtual bool init() = 0;
 
+    /**
+     * @brief ゲートドライバ出力設定
+     * 
+     * @param output 出力値（-1.0 ~ 1.0）
+     * @return true 出力成功
+     * @return false 出力失敗
+     */
     virtual bool output(float output) = 0;
 
     virtual void set_brake_mode(bool enabled) = 0;
