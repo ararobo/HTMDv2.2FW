6065e503d231de553d71285d3943a590d6435d2e
gn10 <8gn24gn25@gmail.com>
2026-02-22 00:59:49 +0900

    CMakeLists及びappの見直し

--- targets/HTMDv2.2c-f303/app/include/app/app.hpp
+++ targets/HTMDv2.2c-f303/app/include/app/app.hpp
@@ -4,12 +4,7 @@
 extern "C" {
 #endif
 
-// Include C headers inside extern "C" to prevent name mangling if they don't have it
-#include "can.h"
-#include "gpio.h"
 #include "main.h"
-#include "tim.h"
-#include "usart.h"
 
 void setup();
 void loop();
